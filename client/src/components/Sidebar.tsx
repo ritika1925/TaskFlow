6e2390226bf6ed047a134e28fd38b73f92b88660
Ritika <ritikatiwari2c25@gmail.com>
2026-06-12 02:22:59 +0530

    sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
@@ -8,13 +9,125 @@ const Sidebar = () => {
       </h1>
 
       <nav className="flex flex-col gap-4">
-        <Link to="/">Dashboard</Link>
-        <Link to="/tasks">Tasks</Link>
-        <Link to="/kanban">Kanban</Link>
-        <Link to="/calendar">Calendar</Link>
-        <Link to="/focus">Focus</Link>
-        <Link to="/analytics">Analytics</Link>
-        <Link to="/settings">Settings</Link>
+        <NavLink
+  to="/dashboard"
+  className={({ isActive }) =>
+    `
+      flex items-center gap-3
+      px-4 py-3 rounded-xl
+      transition
+      ${
+        isActive
+          ? "bg-indigo-600 text-white"
+          : "hover:bg-slate-100"
+      }
+    `
+  }
+>
+  Dashboard
+</NavLink>
+        <NavLink
+  to="/tasks"
+  className={({ isActive }) =>
+    `
+      flex items-center gap-3
+      px-4 py-3 rounded-xl
+      transition
+      ${
+        isActive
+          ? "bg-indigo-600 text-white"
+          : "hover:bg-slate-100"
+      }
+    `
+  }
+>
+  Tasks
+</NavLink>
+        <NavLink
+  to="/kanban"
+  className={({ isActive }) =>
+    `
+      flex items-center gap-3
+      px-4 py-3 rounded-xl
+      transition
+      ${
+        isActive
+          ? "bg-indigo-600 text-white"
+          : "hover:bg-slate-100"
+      }
+    `
+  }
+>
+  Kanban
+</NavLink>
+        <NavLink
+  to="/calendar"
+  className={({ isActive }) =>
+    `
+      flex items-center gap-3
+      px-4 py-3 rounded-xl
+      transition
+      ${
+        isActive
+          ? "bg-indigo-600 text-white"
+          : "hover:bg-slate-100"
+      }
+    `
+  }
+>
+  Calendar
+</NavLink>
+        <NavLink
+  to="/focus"
+  className={({ isActive }) =>
+    `
+      flex items-center gap-3
+      px-4 py-3 rounded-xl
+      transition
+      ${
+        isActive
+          ? "bg-indigo-600 text-white"
+          : "hover:bg-slate-100"
+      }
+    `
+  }
+>
+  Focus
+</NavLink>
+        <NavLink
+  to="/analytics"
+  className={({ isActive }) =>
+    `
+      flex items-center gap-3
+      px-4 py-3 rounded-xl
+      transition
+      ${
+        isActive
+          ? "bg-indigo-600 text-white"
+          : "hover:bg-slate-100"
+      }
+    `
+  }
+>
+  Analytics
+</NavLink>
+        <NavLink
+  to="/settings"
+  className={({ isActive }) =>
+    `
+      flex items-center gap-3
+      px-4 py-3 rounded-xl
+      transition
+      ${
+        isActive
+          ? "bg-indigo-600 text-white"
+          : "hover:bg-slate-100"
+      }
+    `
+  }
+>
+  Settings
+</NavLink>
       </nav>
     </aside>
   );
